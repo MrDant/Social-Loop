f537b9b090373b55696cc8472376b50714b4b97c
MrDant <mael.dantuono@gmail.com>
2023-10-14 13:00:37 +0200

    UI : fix layout overlay

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
   <UContainer class="flex items-center w-full justify-center h-screen relative">
-    <UCard class="text-center"><slot></slot></UCard>
+    <UCard class="text-center relative" :ui="{ base: '' }"><slot></slot></UCard>
   </UContainer>
 </template>
